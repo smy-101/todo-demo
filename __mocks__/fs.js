7712c82c158d91739dac9191ade7e4c1d83b7aa1
smy-101 <shimy1011@foxmail.com>
2022-01-11 13:38:21 +0800

    清除测试副作用

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,40 +1,45 @@
 const fs = jest.genMockFromModule('fs')
 const _fs = jest.requireActual('fs')
 
-Object.assign(fs,_fs)
-const readMocks = {}
+Object.assign(fs, _fs)
 
+let readMocks = {}
 fs.setReadFileMock = (path, error, data) => {
   readMocks[path] = [error, data]
 }
-
-fs.readFile = (path,options,callback)=>{
+fs.readFile = (path, options, callback) => {
   //只传两个参数
-  if (callback===undefined){
+  if (callback === undefined) {
     callback = options
   }
   //如果路径被mock了就直接执行callback回调
-  if (path in readMocks){
+  if (path in readMocks) {
     callback(...readMocks[path])
-  }else {
-    _fs.readFile(path,options,callback)
+  } else {
+    _fs.readFile(path, options, callback)
   }
 }
 
-const writeMocks = {}
-fs.setWriteFileMock = (path,fn)=>{
+
+let writeMocks = {}
+fs.setWriteFileMock = (path, fn) => {
   writeMocks[path] = fn
 }
 
-fs.writeFile = (path,data,options,callback) => {
-  if (callback === undefined){
+fs.writeFile = (path, data, options, callback) => {
+  if (callback === undefined) {
     callback = options
   }
-  if (path in writeMocks){
-    writeMocks[path](path,data,options,callback)
-  }else {
-    _fs.writeFile(path,data,options,callback)
+  if (path in writeMocks) {
+    writeMocks[path](path, data, options, callback)
+  } else {
+    _fs.writeFile(path, data, options, callback)
   }
+}
+
+fs.clearMocks = () => {
+  readMocks = {}
+  writeMocks = {}
 }
 
 module.exports = fs
