fbe262f59c378286b1c96075fa3fc4523d726bf9
smy-101 <shimy1011@foxmail.com>
2022-01-11 09:45:19 +0800

    完善功能  重构代码

--- cli.js
+++ cli.js
@@ -3,22 +3,26 @@ const api = require('./index.js')
 
 program
   .option('-d, --debug', 'output extra debugging')
-  // .option('-s, --small', 'small pizza size')
-  // .option('-p, --pizza-type <type>', 'flavour of pizza');
+// .option('-s, --small', 'small pizza size')
+// .option('-p, --pizza-type <type>', 'flavour of pizza');
 
 program
   .command('add')
   .description('add a task')
   .action((x) => {
     // console.log(x);
-    api.add(x)
-  });
+    api.add(x).then(() => {console.log('添加成功')}, () => {console.log('添加失败')})
+  })
 
 program
   .command('clear')
   .description('clear all tasks')
   .action(() => {
-    console.log('clear');
-  });
+    api.clear().then(() => {console.log('清除成功')}, () => {console.log('清除失败')})
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
+
+if (process.argv.length === 2) {
+  api.showAll()
+}
